80a36062699218cdd518b6f59bfb341559fe4822
belfz <marcinbar1@gmail.com>
2015-12-27 23:25:47 +0100

    added storage driver

--- src/main.js
+++ src/main.js
@@ -2,22 +2,25 @@
 import Rx from 'rx';
 import Cycle from '@cycle/core';
 import {hJSX, makeDOMDriver} from '@cycle/dom';
+import storageDriver from '@cycle/storage';
 import editor from './components/editor';
 
 const content = window.localStorage.getItem('cctext') || '';
 
-function main({DOM}) {
+function main({DOM, storage}) {
   return {
-    DOM: DOM.select('#editor-custom-element').events('save').map(({detail}) => window.localStorage.setItem('cctext', detail)).startWith(content).map(v =>
+    DOM: storage.local.getItem('cctext').startWith('').map(content =>
       <div id="container"> 
         <editor id="editor-custom-element" text={content}/>
       </div>
-      )
+      ),
+    storage: DOM.select('#editor-custom-element').events('save').map(({detail}) => ({key: 'cctext', value: detail}))  
   };
 }
 
 Cycle.run(main, {
   DOM: makeDOMDriver('#app', {
     'editor': editor
-  })
+  }),
+  storage: storageDriver
 });
